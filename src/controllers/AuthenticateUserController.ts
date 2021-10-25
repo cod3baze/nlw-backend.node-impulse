4e42c55b2a7e9b97f669576fb024e6216799cddc
elias alexandre <elliasallex@gmail.com>
2021-10-24 23:56:59 -0300

    feat: handle 401 error on User AuthController

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -3,12 +3,16 @@ import { AuthenticateUserService } from "services/AuthenticateUserService";
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { code } = request.body;
+    try {
+      const { code } = request.body;
 
-    const service = new AuthenticateUserService();
-    const data = await service.execute(code as string);
+      const service = new AuthenticateUserService();
+      const data = await service.execute(code as string);
 
-    return response.status(202).json(data);
+      return response.status(202).json(data);
+    } catch (error) {
+      return response.json(error.message);
+    }
   }
 }
 
