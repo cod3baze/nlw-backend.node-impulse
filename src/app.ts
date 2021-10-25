238e12e936c295ccd8d63fd32b0475bc7fd421a0
elias alexandre <elliasallex@gmail.com>
2021-10-24 23:28:54 -0300

    feat: route to get user access_code on github

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,24 @@ import "dotenv/config";
 
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
+
+app.use(express.json());
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+app.use(routes);
 
 export { app };
